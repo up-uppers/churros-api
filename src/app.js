6a89ff260434e08bbc4ea23d507456e36eb7a8e3
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2022-06-18 22:44:55 -0300

    conexão com banco e rotas para teste

--- src/app.js
+++ src/app.js
@@ -4,15 +4,38 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const bodyParser = require('body-parser')
 
 const app = express()
 
-app.unsubscribe(bodyParser.json())
-app.unsubscribe(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+
+const User = require('./models/User')
 
 app.get('/', (req, res) => {
     res.status(200).json({msg: 'hello world!'})
 })
 
-app.listen(3000)
+app.post('/auth/register', async(req, res) => {
+
+    const { name, email, password, confirmPassword } = req.body
+
+    if(!name){
+        return res.status(422).json({ msg: 'nome obrigatório' })
+    }
+    if(!email){
+        return res.status(422).json({ msg: 'email obrigatório' })
+    }
+    if(!password){
+        return res.status(422).json({ msg: 'senha obrigatório' })
+    }
+})
+
+const dbUser = process.env.DB_USER
+const dbPass = process.env.DB_PASS
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.dkr19.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+    app.listen(3000)
+    console.log('conectou ao banco')
+}).catch((err) => {
+    console.log(err)
+})
